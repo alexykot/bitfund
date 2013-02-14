8696cb13853fed775384f2978195771373bd52f3
alexykot <none@none>
2013-02-14 20:38:17 +0000

    - obsolete user interface removed - obsolete database model removed

--- user interface/prototypes/frontend/create_co_order_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-for(var i = 0; i < 8; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u1'] = 'center';
