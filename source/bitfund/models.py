6589e5841d5f9bd97732bdd297232da4e790cced
alexykot <none@none>
2013-02-14 20:39:10 +0000

    - refactoring done

--- source/bitfund/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.datetime_safe import datetime
-
-YES_NO_CHOICES = (
-    (True, 'Yes'),
-    (False, 'No'),
-)
-
