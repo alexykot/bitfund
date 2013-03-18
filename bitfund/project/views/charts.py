47fe33159315060ce8cd7b368a89c8ff21c85f67
alexykot <none@none>
2013-03-18 18:22:54 +0000

    - charts implementation in progress

--- bitfund/project/views/charts.py
+++ bitfund/project/views/charts.py
@@ -20,12 +20,6 @@ def chart_image(request):
 
 
     TOTAL_DEGREES = 360
-
-    ARGB_DONUT_CHART_PLEDGES = (0.345, 0.435, 0.0196, 1, 'solid')
-    ARGB_DONUT_CHART_REDONATIONS = (0.553, 0.702, 0.0314, 1, 'linear')
-    ARGB_DONUT_CHART_OTHER_SOURCES = (0.937, 0.737, 0.035, 1, 'radial')
-    ARGB_DONUT_CHART_BACKGROUND = (0.929, 0.922, 0.922, 1, 'linear')
-
     plegdes_degrees = 20
     redonations_degrees = 10
     other_sources_degrees = 20
@@ -38,15 +32,9 @@ def chart_image(request):
     background.add_color_stop_rgb(1.0, 0.1, 0.1, 0.1)
 
     data = {'1' : plegdes_degrees, '2' : redonations_degrees, '3' : other_sources_degrees, '4' : background_degrees}
-    radius_list = (6, 6, 4, 2)
+    radius_list = (12, 12, 8, 4)
     shadow_parts = (True, True, False, False)
     colors = [ARGB_DONUT_CHART_PLEDGES, ARGB_DONUT_CHART_REDONATIONS, ARGB_DONUT_CHART_OTHER_SOURCES, ARGB_DONUT_CHART_BACKGROUND]
-
-
-# RGBCOLOR_DONUT_CHART_PLEDGES = '586F05'
-# RGBCOLOR_DONUT_CHART_REDONATIONS = '8DB308'
-# RGBCOLOR_DONUT_CHART_OTHER_SOURCES = 'EFBC09'
-# RGBCOLOR_DONUT_CHART_BACKGROUND = 'EDEBEA'
 
     cairoplot.donut_plot(chart_abspathname, data, 900, 900, background='transparent',
                          gradient=True,
@@ -55,30 +43,6 @@ def chart_image(request):
                          inner_radius=1,
                          colors=colors,
                          radius_list=radius_list)
-
-    # colors_mix = [ 'red',                     #R
-    #                (0, 1, 0, 0.75, 'solid'),  #G
-    #                (1, 1, 0, 0.75, 'linear'), #Y
-    #                (0, 0, 1, 0.75, 'radial')] #B
-
-    # cairoplot.donut_plot('donut_transp.png', data, 800, 600, background = 'transparent',
-    #                      gradient = True,
-    #                      shadow = True,
-    #                      shadow_parts = shadow_parts, inner_radius = 0.8,
-    #                      colors = colors_mix, radius_list = radius_list)
-    #
-    # cairoplot.donut_plot('donut_gradient_off.png', data, 800, 600, background = 'transparent',
-    #                      gradient = False,
-    #                      shadow = True,
-    #                      shadow_parts = shadow_parts, inner_radius = 0.8,
-    #                      colors = colors, radius_list = radius_list)
-    #
-    # cairoplot.donut_plot('donut_gradient_background.png', data, 800, 600, background = background,
-    #                      gradient = False,
-    #                      shadow = True,
-    #                      shadow_parts = shadow_parts, inner_radius = 0.8,
-    #                      colors = colors, radius_list = radius_list)
-
 
     response = HttpResponse(mimetype='image/png')
     response['Content-Length'] = os.path.getsize(chart_abspathname)
