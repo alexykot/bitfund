269c2e605d364aad457561c487d0f49078c8cf7c
alexykot@alexy-D620 <alexykot@alexy-D620>
2013-04-23 01:21:38 +0100

    - small fractional added

--- bitfund/core/templatetags/bitfund_filters.py
+++ bitfund/core/templatetags/bitfund_filters.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, ROUND_DOWN
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -7,5 +8,14 @@ register = template.Library()
 @stringfilter
 def upto(value, delimiter=None):
     return value.split(delimiter)[0]
+
+@register.filter
+@stringfilter
+def fract2int(value):
+    value = Decimal(value)
+    fractional_part = value-value.quantize(Decimal('0'), rounding=ROUND_DOWN)
+    fractional_part = str(fractional_part)[2:]
+    return fractional_part
+
 upto.is_safe = True
 
